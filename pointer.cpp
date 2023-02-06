f72d5827a3a35fbcdd6814b33de4cd5f4ea90016
Gitviki <sudovicky@gmail.com>
2023-02-06 23:39:08 +0530

    o

---
+++ pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5, *b, *c, d=2, e;
+    
+    cout<<a<<endl;
+    b= &a;
+    cout<<b<<endl;
+    c= b;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    c=&d;
+    cout<<c<<endl;
+    e = 0x16efdb158 ;
+    cout<<e<<endl;
+
+
+
+
+    return 0;
+}
